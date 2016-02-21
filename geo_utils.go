ef95ea2178323d3456a0f30a37e5624696f70727
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-20 22:41:02 -0500

    Geo utils

---
+++ geo_utils.go
@@ -0,0 +1,50 @@
+package ggsearch
+
+import (
+	"math"
+)
+
+// Earth radius in miles
+const R = 3959
+
+func DegreesToRadians(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
+
+func Haversine(rad_lat1, rad_lng1, rad_lat2, rad_lng2 float64) float64 {
+	delta_lambda := rad_lng2 - rad_lng1
+	delta_phi := rad_lat2 - rad_lat1
+	phi_m := (rad_lat1 + rad_lat2) / 2
+	x := delta_lambda * math.Cos(phi_m)
+	y := delta_phi
+	hfo := x*x + y*y
+	return R * math.Sqrt(hfo)
+}
+
+func RadLatToGrid(rad_lat float64, lat_tiles int) int {
+	return int((rad_lat + math.Pi/2) * float64(lat_tiles) / math.Pi)
+}
+
+func RadLngToGrid(rad_lng float64, lng_tiles int) int {
+	return int((rad_lng + math.Pi) * float64(lng_tiles) / math.Pi / 2)
+}
+
+func ClampGridLat(grid_lat, lat_tiles int) int {
+	if grid_lat >= lat_tiles {
+		grid_lat -= 1
+	}
+	if grid_lat < 0 {
+		grid_lat = 0
+	}
+	return grid_lat
+}
+
+func WrapGridLng(grid_lng, lng_tiles int) int {
+	for grid_lng >= lng_tiles {
+		grid_lng -= lng_tiles
+	}
+	for grid_lng < 0 {
+		grid_lng += lng_tiles
+	}
+	return grid_lng
+}
---
+++ geo_utils_test.go
@@ -0,0 +1,49 @@
+package ggsearch
+
+import (
+	"math"
+	. "gopkg.in/check.v1"
+	"testing"
+	"github.com/stretchr/testify/assert" 
+)
+
+// Hook up gocheck into the "go test" runner.
+func TestGeoUtils(t *testing.T) { TestingT(t) }
+
+type GeoUtilsSuite struct {
+}
+
+var _ = Suite(&GeoUtilsSuite{})
+
+func (s *GeoUtilsSuite) TestDegreesToRadiansZero(c *C) {
+	c.Assert(DegreesToRadians(0), Equals, 0.0)
+}
+
+func (s *GeoUtilsSuite) TestDegreesToRadiansHalfPi(c *C) {
+	c.Assert(DegreesToRadians(90), Equals, math.Pi/2)
+}
+
+func (s *GeoUtilsSuite) TestDegreesToRadiansPi(c *C) {
+	c.Assert(DegreesToRadians(180), Equals, math.Pi)
+}
+
+func (s *GeoUtilsSuite) TestClampGridLat(c *C) {
+	c.Assert(ClampGridLat(10, 11), Equals, 10)
+	c.Assert(ClampGridLat(11, 11), Equals, 10)
+	c.Assert(ClampGridLat(0, 11), Equals, 0)
+	c.Assert(ClampGridLat(-1, 11), Equals, 0)
+}
+
+func (s *GeoUtilsSuite) TestWrapGridLng(c *C) {
+	c.Assert(WrapGridLng(10, 11), Equals, 10)
+	c.Assert(WrapGridLng(11, 11), Equals, 0)
+	c.Assert(WrapGridLng(0, 11), Equals, 0)
+	c.Assert(WrapGridLng(-1, 11), Equals, 10)
+}
+
+func TestHaversine(t *testing.T) {
+	assert := assert.New(t)
+	distance := Haversine(50*math.Pi/180, -70*math.Pi/180,
+		51*math.Pi/180, -71*math.Pi/180)
+	assert.InDelta(distance, 81.89, 0.01)
+}
