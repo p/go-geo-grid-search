9609b11167bab22c9f58a0a6190cf412583faa9a
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-23 22:28:10 -0500

    Document Locatable

--- locatable.go
+++ locatable.go
@@ -1,6 +1,11 @@
 package ggsearch
 
+/*
+ggsearch stores and queries objects implementing Locatable interface.
+*/
 type Locatable interface {
+	// Returns latitude of this object in degrees
 	Lat() float64
+	// Returns longitude of this object in degrees
 	Lng() float64
 }
