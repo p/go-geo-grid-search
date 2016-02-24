2b00142cf31aa4ff00bf765ad60639e79892bf47
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-23 22:47:26 -0500

    Note performance

--- doc.go
+++ doc.go
@@ -7,6 +7,11 @@ The distinguishing feature of ggsearch is that it does not require a
 bounding box for querying. ggsearch builds a sparse grid index of the places
 and performs queries by iterating the grid tiles in a spiral fashion.
 
+Performance
+
+ggsearch was built for speed. On a t2.micro AWS instance a Web service
+running ggsearch queried a data set of 40,000 places in 5 ms/request.
+
 Caveats
 
 ggsearch is built for querying populated areas and as such querying
